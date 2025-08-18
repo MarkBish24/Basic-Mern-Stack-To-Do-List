fabd9ecaf1ef156540fe411afa07e77ccf5b8bae
Mark Bishoff <markbish24@gmail.com>
2025-08-18 08:45:21 -0700

    sending data

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -2,22 +2,32 @@ import { useState, useEffect } from "react";
 import Navbar from "../components/Navbar";
 import RateLimitedUI from "../components/RateLimitedUI";
 import axios from "axios";
+import toast from "react-hot-toast";
 
 const HomePage = () => {
-  const [isRateLimited, setIsRateLimited] = useState(true);
+  const [isRateLimited, setIsRateLimited] = useState(false);
   const [notes, setNotes] = useState([]);
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     const fetchNotes = async () => {
       try {
-        const data = await axios.get("http://localhost:5001/api/notes");
-
-        console.log(data);
+        const res = await axios.get("http://localhost:5001/api/notes");
+        console.log(res.data);
+        setNotes(res.data);
+        setIsRateLimited(false);
       } catch (error) {
         console.log("Error fetching notes");
+        if (error.response.status == 429) {
+          setIsRateLimited(true);
+        } else {
+          toast.error("Failed to load notes");
+        }
+      } finally {
+        setLoading(false);
       }
     };
+    fetchNotes();
   }, []);
 
   return (
