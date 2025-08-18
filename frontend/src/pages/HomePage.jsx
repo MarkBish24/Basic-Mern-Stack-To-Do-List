415a32b6b8101c7e370d0fae4ab60d79d57fb8bb
Mark Bishoff <markbish24@gmail.com>
2025-08-18 08:15:39 -0700

    created navbar

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,7 +1,11 @@
-import React from "react";
+import Navbar from "../components/Navbar";
 
 const HomePage = () => {
-  return <div>Home Page</div>;
+  return (
+    <div className="min-h-screen">
+      <Navbar />
+    </div>
+  );
 };
 
 export default HomePage;
